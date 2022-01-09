4666b3dae038a0add58347e19e2afac19eba37e1
sumit <sumitthapa90@gmail.com>
2022-01-09 15:00:42 +0530

    block-BRaaao

---
+++ block-BRaaao/code/news-app/src/componet/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <a className="logo">Articles</a>
+
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+export default Header;
