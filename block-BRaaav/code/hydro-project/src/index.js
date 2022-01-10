50ac5c3c53039cc3287ac63a797fa8357dab2d78
sumit <sumitthapa90@gmail.com>
2022-01-11 00:05:33 +0530

    block-BRaaav

---
+++ block-BRaaav/code/hydro-project/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import "./assets/stylesheet/style.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
