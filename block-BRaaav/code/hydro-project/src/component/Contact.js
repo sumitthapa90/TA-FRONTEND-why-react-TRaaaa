5aae5a3031af2327cf0703e2f8d57c2bfb4cf2e9
sumit <sumitthapa90@gmail.com>
2022-01-11 12:19:49 +0530

    block-BRaaav

--- block-BRaaav/code/hydro-project/src/component/Contact.js
+++ block-BRaaav/code/hydro-project/src/component/Contact.js
@@ -60,7 +60,7 @@ function Contact() {
               title="map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3375.9195155713787!2d76.35140991561741!3d32.206399320166405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391b530e24726e0d%3A0x71ff0cae0784712d!2sAltCampus%20Services%20Pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1600510560646!5m2!1sen!2sin"
               frameborder="0"
-              style="border: 0"
+              style={{ border: 0 }}
               allowfullscreen=""
               aria-hidden="false"
               tabindex="0"
