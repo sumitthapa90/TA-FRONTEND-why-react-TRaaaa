1fc8f3234992d498cc9e1735e714246519ec72f1
sumit <sumitthapa90@gmail.com>
2022-01-11 21:51:53 +0530

    block-BRaaax.

--- block-BRaaax/code/button/src/component/App.js
+++ block-BRaaax/code/button/src/component/App.js
@@ -3,15 +3,11 @@ import Button from "./Button";
 function App() {
   return (
     <>
-      <Button label="Click Me!" type="secondary" size="large" />
-      <Button label="Click Me!" type="primary" size="medium" />
-      <Button label="Click Me!" type="secondary" size="small" />
-      <Button label="Click Me!" />
-      <Button
-        disabled
-        type="secondary"
-        onClickHandler={() => alert("You clicked me!")}
-      />
+    <Button label="Click Me!" type="secondary" size="large"/>
+    <Button label="Click Me!" type="primary" size="medium"/>
+    <Button label="Click Me!" type="secondary" size="small"/>
+    <Button label="Click Me!"/>
+    <Button disabled type="secondary" onClickHandler={()=>alert('You clicked me!')}/>
     </>
   );
 }
